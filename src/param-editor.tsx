a9f6b72733240b34f723f9c25379a655c2b19dd4
Dmitrii Samsonenko <samsondm97@gmail.com>
2026-01-31 19:50:26 +0300

    feat: implement model updating

--- src/param-editor.tsx
+++ src/param-editor.tsx
@@ -30,11 +30,23 @@ interface State {
 }
 
 export class ParamEditor extends Component<Props, State> {
+  public getModel(): Model {
+    return this.state.model;
+  }
+
   constructor(props: Props) {
     super(props);
     this.state = {
       model: this.props.model,
     };
+  }
+
+  private handleChange(e: ChangeEvent<HTMLInputElement, HTMLInputElement>) {
+    const param = this.state.model.paramValues.find(
+      (value) => value.paramId.toString() === e.target.id,
+    );
+    if (!param) return;
+    param.value = e.target.value;
   }
 
   private getDefaultValue(id: number) {
@@ -53,6 +65,7 @@ export class ParamEditor extends Component<Props, State> {
               <label htmlFor={`${param.id}`}>{param.name}</label>
               <input
                 id={`${param.id}`}
+                onChange={(e) => this.handleChange(e)}
                 type="text"
                 defaultValue={this.getDefaultValue(param.id)}
                 name={param.name}
@@ -60,6 +73,15 @@ export class ParamEditor extends Component<Props, State> {
             </>
           );
         })}
+
+        <button
+          onClick={() => {
+            const result = this.getModel();
+            console.log(result.paramValues);
+          }}
+        >
+          Get Model
+        </button>
       </section>
     );
   }
